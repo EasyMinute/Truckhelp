c1623b31e1ceb1a84af902843e3b94ad7dc8487e
EasyMinute <nikyura@gmail.com>
2024-08-13 18:03:07 +0300

    Forms and 404

--- wp-content/themes/proacto-theme/404.php
+++ wp-content/themes/proacto-theme/404.php
@@ -11,9 +11,22 @@ get_header();
 
 ?>
 
-	<main class="404-template">
-		404
-	</main><!-- #main -->
+	<section class="block-404">
+        <div class="container">
+            <div class="block-404__wrap">
+                <h2 class="headline headline-h2 bold">
+                    <?= __('Сторінку не знайдено', 'proacto') ?>
+                </h2>
+                <p class="body body-s regular">
+                    <?= __('Можливо, ця сторінка була видалена або припущена помилка в адресі.', 'proacto') ?>
+                </p>
+                <a href="<?= get_home_url() ?>" class="button button-l primary">
+	                <?= __('Перейти на головну сторінку', 'proacto') ?>
+                </a>
+                <img src="<?= get_template_directory_uri() . '/assets/img/static/404.png' ?>" alt="">
+            </div>
+        </div>
+    </section>
 
 <?php
 get_footer();
