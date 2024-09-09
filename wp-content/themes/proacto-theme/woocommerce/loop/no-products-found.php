3165f443c9d1e54e8c107cb2df1066654cdef02c
EasyMinute <nikyura@gmail.com>
2024-09-09 18:05:58 +0300

    Containered archive

--- wp-content/themes/proacto-theme/woocommerce/loop/no-products-found.php
+++ wp-content/themes/proacto-theme/woocommerce/loop/no-products-found.php
@@ -18,6 +18,8 @@
 defined( 'ABSPATH' ) || exit;
 
 ?>
-<div class="woocommerce-no-products-found">
-	<?php wc_print_notice( esc_html__( 'No products were found matching your selection.', 'woocommerce' ), 'notice' ); ?>
+<div class="container">
+    <div class="woocommerce-no-products-found">
+        <?php wc_print_notice( esc_html__( 'No products were found matching your selection.', 'woocommerce' ), 'notice' ); ?>
+    </div>
 </div>
